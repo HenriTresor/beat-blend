ffd228187d2bcbc5d9cdc4e18433347f4488f9b7
henritresor <shimwamanahenritresor@gmail.com>
2023-05-03 22:09:21 +0200

    added authentication with spotify

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -2,7 +2,9 @@ import { Router } from "express";
 import querystring from 'querystring'
 import crypto from 'crypto'
 import { config } from 'dotenv'
-
+// import SpotifyWebApiNode from 'spotify-web-api-node'
+import request from "request";
+import { Buffer } from "buffer";
 
 config()
 
@@ -11,10 +13,35 @@ const router = Router()
 
 router.get('/login', (req, res) => {
 
-    const state = crypto.randomBytes(64).toString('hex')
-    const scope = 'user-read-private user-read-email'
+    const state = crypto.randomBytes(16).toString('hex')
+    const scope = 'user-read-private user-read-email streaming'
+    res.redirect(`https://accounts.spotify.com/authorize?${querystring.stringify({ client_id: process.env.CLIENT_ID, scope, state, redirect_uri, response_type: 'code' })}`)
+})
 
-    res.redirect(`https://accounts.spotify.com/authorize?${querystring.stringify({ scope, state, redirect_uri, client_id: process.env.CLIENT_ID })}`)
+router.get('/callback', async (req, res) => {
+    let code = req.query.code || null
+    let state = req.query.state || null
+
+    const authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+            code,
+            redirect_uri: 'http://localhost:6060/callback',
+            grant_type: 'authorization_code',
+
+        },
+        headers: {
+            'Authorization': 'Basic ' + Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')
+        }
+    }
+
+    request.post(authOptions, (err, response, body) => {
+        if (err) {
+            res.status(500).json(err)
+            return
+        }
+        res.status(200).json(JSON.parse(body))
+    })
 })
 
 export default router
